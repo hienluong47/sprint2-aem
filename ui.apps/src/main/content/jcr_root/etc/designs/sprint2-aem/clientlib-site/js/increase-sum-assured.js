305ed0ec859af61ee0ec5c984257e67137832c8d
caingocduong <caingocudong2606@gmail.com>
2018-05-02 10:34:02 +0700

    Edit increase sum assured

--- ui.apps/src/main/content/jcr_root/etc/designs/sprint2-aem/clientlib-site/js/increase-sum-assured.js
+++ ui.apps/src/main/content/jcr_root/etc/designs/sprint2-aem/clientlib-site/js/increase-sum-assured.js
@@ -1,11 +1,27 @@
 ;(function ($, window, undefined) {
     'use strict';
     var pluginName = 'increase-sum-assured',
-        lengthField = 2;
+        pos_sr_url = '/content/sprint2-aem/pos-service-requests.html',
+        lengthField = 2,
+        path = '/etc/designs/sprint2-aem/clientlib-common/';
+
+    var events = {
+        click: 'click.' + pluginName,
+    };
+
+    var createPopup = function(popupObj) {
+        var button = popupObj.confirm ? '<button class="btn btn-default ' + popupObj.confirm.class + ' margin-left-xxs" type="button" data-toggle="modal" data-dismiss="modal" data-id="' + popupObj.confirm.id + '">' + popupObj.confirm.title + '</button>' : '',
+            popup = $('body').find('[data-id=common-popup]');
+        popup.html('<div class="modal-dialog partial-screen-modal-dialog"><div class="modal-content overflow-auto"><div class="modal-header bg-t2"><button class="modal-header-btn pull-left" data-dismiss="modal"><svg class="icon-xs hide-on-fallback" role="img" title="closewhite-glyph"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="'+ path +'icons/icons.svg#closewhite-glyph"></use><image class="icon-fallback" alt="closewhite-glyph" src="'+ path +'closewhite-glyph.png" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href=""></image></svg>  </button><h6 class="w text-center">' + popupObj.title + '</h6></div><div class="modal-container padding-top-xxl content-centering"><p class="content-centering bt2">' + popupObj.description + '</p><div class="margin-bottom-l margin-top-xxl"><button style="margin-left:240px;" class="btn btn-default ' + popupObj.decline.class + '" type="button" data-dismiss="modal" data-id="' + popupObj.decline.id + '">' + popupObj.decline.title + '</button>' + button + '</div></div></div></div>');
+        popup.off('hidden.bs.modal').on('hidden.bs.modal', popupObj.decline.func);
+        popupObj.confirm && $('body').off(events.click, '[data-id=' + popupObj.confirm.id + ']').on(events.click, '[data-id=' + popupObj.confirm.id + ']', popupObj.confirm.func);
+        popup.modal('show');
+    };
 
     function IncreaseSumAssured(element, options) {
         this.element = $(element);
         this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+        console.log('options ', this.element.siblings('[data-id=select-btn]'));
         this.init();
     };
 
@@ -14,21 +30,50 @@
             var that = this;
             that.initDOM();
             that.getPolicyDetail();
+            // that.myFirstHandlebars();
+            that.element.submit(function (e) {
+               e.preventDefault();
+               createPopup({
+                   title: '',
+                   description: 'Submit successfully',
+                   decline: {
+                       id: 'ok',
+                       title: 'OK',
+                       class: 'btn-secondary',
+                       func: function () {
+                           window.location.href = pos_sr_url;
+                       }
+                   }
+               });
+            });
         },
 
         initDOM : function () {
 
         },
 
+        myFirstHandlebars : function() {
+			let templateExample = 
+                "<div class='entry'>"+
+                    "<h3>{{ title }}</h3>"+
+                    "<div>"+
+                        "{{ body }}"+
+                    "</div>"+
+                "</div>";
+            let context = { title: "My new post", body : "This is my first post!"};
+            let htmlCompile = window.Handlebars.compile(templateExample)(context);
+            $('#handlebars-example').html(htmlCompile);
+        },
+
         getPolicyDetail : function () {
             var that = this;
           $.ajax({
-              url : 'http://localhost:3000/policies/12654354',
+              url : 'http://localhost:3000/policies/'+document.getElementById('policy-number').value,
               method : 'GET',
               type : 'json',
               success : function (response) {
                   $('#existing-sum-basic').append(response.sumAssured);
-                  $('[data-id=policy]').append('P12654354');
+                  $('[data-id=policy]').append('P'+document.getElementById('policy-number').value);
                   let sumAssuredErrorLabelId = '#sum-basic-error';
                   let sumAssuredInputId = '#input-sum-basic';
                   that.checkSumAssured(sumAssuredErrorLabelId, sumAssuredInputId, response.sumAssured, response.maxSA); 
